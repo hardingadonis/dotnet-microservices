8931201aa01002c08056f92b06b4e97e7fd0cdbc
Minh Vương <hardingadonis@gmail.com>
2024-07-18 14:40:17 +0700

    [Add] Implement `Repositories` for `Catalog`

---
+++ src/Services/Catalog/Catalog.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Domain.Entities;
+
+namespace Catalog.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAll();
+
+        Task<Product?> GetById(string id);
+
+        Task<IEnumerable<Product>> GetProductByName(string name);
+
+        Task<Product> Create(Product product);
+
+        Task<Product> Update(Product product);
+
+        Task<bool> Delete(string id);
+    }
+}
