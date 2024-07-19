01e344bdeacdd92682c52b5ebad61d008691482b
Minh Vương <hardingadonis@gmail.com>
2024-07-19 15:00:54 +0700

    [Add] Implement `Mapper` for `Catalog.Application`

---
+++ src/Services/Catalog/Catalog.Application/Mappers/CatalogMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Domain.Entities;
+
+namespace Catalog.Application.Mappers
+{
+    public class CatalogMappingProfile : Profile
+    {
+        public CatalogMappingProfile()
+        {
+            CreateMap<Brand, BrandResponse>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Application/Responses/BrandResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Application.Responses
+{
+    public class BrandResponse
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+    }
+}
