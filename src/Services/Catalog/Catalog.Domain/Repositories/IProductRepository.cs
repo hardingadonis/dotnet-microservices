27b83a54d7ff32d4ddb4c12a6c7248bbe8332d57
Minh Vương <hardingadonis@gmail.com>
2024-07-19 10:55:57 +0700

    [Add] Implement `Repositories` for `Catalog.Infrastructure`

--- src/Services/Catalog/Catalog.Domain/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.Domain/Repositories/IProductRepository.cs
@@ -8,12 +8,12 @@ namespace Catalog.Domain.Repositories
 
         Task<Product?> GetById(string id);
 
-        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetByName(string name);
 
         Task<Product> Create(Product product);
 
-        Task<Product> Update(Product product);
+        Task<bool> Update(Product product);
 
-        Task<bool> Delete(string id);
+        Task<bool> DeleteById(string id);
     }
 }
