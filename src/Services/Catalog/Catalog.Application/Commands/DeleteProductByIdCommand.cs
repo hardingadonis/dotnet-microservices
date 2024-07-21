5d3388e8cd48be6aed1667c59e2f1b1b5f679166
Minh Vương <hardingadonis@gmail.com>
2024-07-21 14:38:21 +0700

    [Add] Implemented delete product by ID functionality

---
+++ src/Services/Catalog/Catalog.Application/Commands/DeleteProductByIdCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Catalog.Application.Commands
+{
+    public record DeleteProductByIdCommand(string Id) : IRequest<bool>;
+}
---
+++ src/Services/Catalog/Catalog.Application/Commands/Handlers/DeleteProductByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Application.Exceptions;
+using Catalog.Domain.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Commands.Handlers
+{
+    public class DeleteProductByIdHandler : IRequestHandler<DeleteProductByIdCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> Handle(DeleteProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetById(request.Id);
+
+            if (product == null)
+            {
+                throw new ProductNotFoundException($"id: {request.Id}");
+            }
+
+            return await _productRepository.DeleteById(product.Id);
+        }
+    }
+}
