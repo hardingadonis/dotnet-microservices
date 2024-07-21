002076cc4d2ce6687eb3a9a8829143ebd075e85b
Minh Vương <hardingadonis@gmail.com>
2024-07-21 14:33:01 +0700

    [Add] Implemented product update and creation functionality

--- src/Services/Catalog/Catalog.Application/Mappers/CatalogMappingProfile.cs
+++ src/Services/Catalog/Catalog.Application/Mappers/CatalogMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Catalog.Application.Commands;
 using Catalog.Application.Responses;
 using Catalog.Domain.Entities;
 
@@ -11,6 +12,16 @@ namespace Catalog.Application.Mappers
             CreateMap<Brand, BrandResponse>().ReverseMap();
             CreateMap<Category, CategoryResponse>().ReverseMap();
             CreateMap<Product, ProductResponse>().ReverseMap();
+
+            CreateMap<CreateProductCommand, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Brand, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.Ignore());
+
+            CreateMap<UpdateProductCommand, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Brand, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.Ignore());
         }
     }
 }
