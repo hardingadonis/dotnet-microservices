f5a62b0576b78435b5d33a3c2291cbc8035a42c3
Minh Vương <hardingadonis@gmail.com>
2024-07-23 13:42:56 +0700

    [Add] Create `CatalogSpecParams.cs` & `Pagination.cs`

---
+++ src/Services/Catalog/Catalog.Domain/Specs/CatalogSpecParams.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.Domain.Specs
+{
+    public class CatalogSpecParams
+    {
+        public const int MaxPageSize = 70;
+
+        public int PageIndex { get; set; } = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+
+        public string? BrandId { get; set; }
+
+        public string? CategoryId { get; set; }
+
+        public string? Sort { get; set; }
+
+        public string? Search { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Domain/Specs/Pagination.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.Domain.Specs
+{
+    public class Pagination<T> where T : class
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public long Count { get; set; }
+
+        public IReadOnlyCollection<T> Data { get; set; } = Array.Empty<T>();
+
+        public Pagination()
+        {
+        }
+
+        public Pagination(int pageIndex, int pageSize, long count, IReadOnlyCollection<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            Count = count;
+            Data = data;
+        }
+    }
+}
