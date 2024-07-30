b1d9fb1927daa3fa4d4bc452494e8d59ac154ef4
Minh Vương <hardingadonis@gmail.com>
2024-07-30 14:31:58 +0700

    [Add] Implement `BasketMapper` and `BasketMappingProfile`

---
+++ src/Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Domain.Entities;
+
+namespace Basket.Application.Mappers
+{
+    public class BasketMappingProfile : Profile
+    {
+        public BasketMappingProfile()
+        {
+            CreateMap<ShoppingCart, BasketResponse>().ReverseMap();
+            CreateMap<ShoppingCartItem, BasketItemResponse>().ReverseMap();
+        }
+    }
+}
