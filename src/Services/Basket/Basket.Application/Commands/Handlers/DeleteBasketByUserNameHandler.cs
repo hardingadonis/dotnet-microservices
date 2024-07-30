5ca77a3c2ace6f50ff7183044850a8107f6eda7d
Minh Vương <hardingadonis@gmail.com>
2024-07-30 14:41:25 +0700

    [Add] Implement all commands & queries

---
+++ src/Services/Basket/Basket.Application/Commands/Handlers/DeleteBasketByUserNameHandler.cs
@@ -0,0 +1,21 @@
+﻿using Basket.Application.Exceptions;
+using Basket.Domain.Repositories;
+using MediatR;
+
+namespace Basket.Application.Commands.Handlers
+{
+    public class DeleteBasketByUserNameHandler : IRequestHandler<DeleteBasketByUserNameCommand, bool>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public DeleteBasketByUserNameHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        public async Task<bool> Handle(DeleteBasketByUserNameCommand request, CancellationToken cancellationToken)
+        {
+            return await _basketRepository.DeleteBasket(request.UserName) ? true : throw new DeleteBasketException(request.UserName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.Application/Queries/Handlers/GetBasketByNameHandler.cs
@@ -0,0 +1,31 @@
+﻿using Basket.Application.Exceptions;
+using Basket.Application.Mappers;
+using Basket.Application.Responses;
+using Basket.Domain.Entities;
+using Basket.Domain.Repositories;
+using MediatR;
+
+namespace Basket.Application.Queries.Handlers
+{
+    public class GetBasketByNameHandler : IRequestHandler<GetBasketByUserNameQuery, BasketResponse>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public GetBasketByNameHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        public async Task<BasketResponse> Handle(GetBasketByUserNameQuery request, CancellationToken cancellationToken)
+        {
+            var basket = await _basketRepository.GetBasket(request.UserName);
+
+            if (basket != null)
+            {
+                return BasketMapper.Mapper.Map<ShoppingCart, BasketResponse>(basket);
+            }
+
+            throw new BasketNotFoundException(request.UserName);
+        }
+    }
+}
