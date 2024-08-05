c3533e234c4c1d371fdcc53b6fa278cd7efb3345
Minh Vương <hardingadonis@gmail.com>
2024-08-05 09:36:10 +0700

    [Add] `BasketCheckout` implementation

---
+++ src/Services/Basket/Basket.Domain/Entities/BasketCheckout.cs
@@ -0,0 +1,33 @@
+﻿namespace Basket.Domain.Entities
+{
+    public class BasketCheckout
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+
+        public string AddressLine { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public string ZipCode { get; set; } = string.Empty;
+
+        public string CardName { get; set; } = string.Empty;
+
+        public string CardNumber { get; set; } = string.Empty;
+
+        public string Expiration { get; set; } = string.Empty;
+
+        public string CVV { get; set; } = string.Empty;
+
+        public int PaymentMethod { get; set; }
+    }
+}
