24af8102483d906ad08dac380bf2973dcb514030
Minh Vương <hardingadonis@gmail.com>
2024-08-05 09:14:17 +0700

    [Update] Add check id in `UpdateProductHandler`

--- src/Services/Catalog/Catalog.Application/Commands/Handlers/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.Application/Commands/Handlers/UpdateProductHandler.cs
@@ -25,27 +25,26 @@ namespace Catalog.Application.Commands.Handlers
 
         public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
         {
-            // Get brand, category and product by id
+            if (!request.Id.IsValidHex24Id())
+            {
+                throw new InvalidIdException(request.Id);
+            }
+
             var brandTask = _brandRepository.GetById(request.BrandId);
             var categoryTask = _categoryRepository.GetById(request.CategoryId);
             var productTask = _productRepository.GetById(request.Id);
 
-            // Wait for all tasks to complete
             await Task.WhenAll(brandTask, categoryTask, productTask);
 
-            // If product is not found, throw exception
             _ = await productTask ?? throw new ProductNotFoundException($"id '{request.Id}'");
 
-            // If brand or category is not found, throw exception
             var brand = await brandTask ?? throw new BrandNotFoundException(request.BrandId);
             var category = await categoryTask ?? throw new CategoryNotFoundException(request.CategoryId);
 
-            // Map request to product
             var product = CatalogMapper.Mapper.Map<Product>(request);
             product.Category = category;
             product.Brand = brand;
 
-            // Update product
             return await _productRepository.Update(product);
         }
     }
