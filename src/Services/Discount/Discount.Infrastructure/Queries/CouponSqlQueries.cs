d489a5651ca142198dd7ca59cdfafc960efd306d
Minh Vương <hardingadonis@gmail.com>
2024-08-06 10:29:04 +0700

    [Add] Implement repository for `Discount.Infrastructure`

---
+++ src/Services/Discount/Discount.Infrastructure/Queries/CouponSqlQueries.cs
@@ -0,0 +1,24 @@
+﻿namespace Discount.Infrastructure.Queries
+{
+    public static class CouponSqlQueries
+    {
+        public const string GetCoupon = @"
+            SELECT *
+            FROM Coupon
+            WHERE ProductName = @ProductName";
+
+        public const string CreateCoupon = @"
+            INSERT INTO Coupon (ProductName, Description, Amount)
+            VALUES (@ProductName, @Description, @Amount)
+            RETURNING *";
+
+        public const string UpdateCoupon = @"
+            UPDATE Coupon
+            SET ProductName = @ProductName, Description = @Description, Amount = @Amount
+            WHERE Id = @Id";
+
+        public const string DeleteCoupon = @"
+            DELETE FROM Coupon
+            WHERE ProductName = @ProductName";
+    }
+}
---
+++ src/Services/Discount/Discount.Infrastructure/Repositories/DiscountRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Discount.Domain.Entities;
+using Discount.Domain.Repositories;
+using Discount.Infrastructure.Queries;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.Infrastructure.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly string _connectionString;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["DatabaseSettings:ConnectionString"]
+                ?? throw new ArgumentNullException("The PostgreSQL connection string is missing in the configuration. Please provide a valid connection string.");
+        }
+
+        private static async Task<Coupon?> IsExistingCoupon(string productName, NpgsqlConnection connection)
+        {
+            return await connection.QueryFirstOrDefaultAsync<Coupon?>(CouponSqlQueries.GetCoupon, new { ProductName = productName });
+        }
+
+        public async Task<Coupon?> CreateDiscount(Coupon coupon)
+        {
+            await using var connection = new NpgsqlConnection(_connectionString);
+
+            return await connection.QueryFirstOrDefaultAsync<Coupon>(CouponSqlQueries.CreateCoupon, coupon);
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            await using var connection = new NpgsqlConnection(_connectionString);
+
+            var deletedCoupon = await connection.ExecuteAsync(CouponSqlQueries.DeleteCoupon, new { ProductName = productName });
+
+            return deletedCoupon > 0;
+        }
+
+        public async Task<Coupon?> GetDiscount(string productName)
+        {
+            await using var connection = new NpgsqlConnection(_connectionString);
+
+            return await IsExistingCoupon(productName, connection);
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            await using var connection = new NpgsqlConnection(_connectionString);
+
+            var existingCoupon = await IsExistingCoupon(coupon.ProductName, connection);
+
+            if (existingCoupon is null)
+            {
+                return false;
+            }
+
+            coupon.Id = existingCoupon.Id;
+
+            var updatedCoupon = await connection.ExecuteAsync(CouponSqlQueries.UpdateCoupon, coupon);
+
+            return updatedCoupon > 0;
+        }
+    }
+}
