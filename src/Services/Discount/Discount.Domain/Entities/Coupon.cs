99d4fa54db9a9fc534953aac5a2d79934c50737a
Minh Vương <hardingadonis@gmail.com>
2024-08-06 09:32:04 +0700

    [Add] Add entities & interface repository for `Discount.Domain`

---
+++ src/Services/Discount/Discount.Domain/Entities/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace Discount.Domain.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public int Amount { get; set; }
+    }
+}
