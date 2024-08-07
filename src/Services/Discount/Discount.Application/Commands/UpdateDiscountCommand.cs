238ecccb0463f9df535bc1bdb2c4ab158d89ffd7
Minh Vương <hardingadonis@gmail.com>
2024-08-07 14:37:00 +0700

    [Add] Implement update discount command

---
+++ src/Services/Discount/Discount.Application/Commands/UpdateDiscountCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Discount.Application.Commands
+{
+    public record UpdateDiscountCommand(
+        int Id,
+        string ProductName,
+        string Description,
+        int Amount
+    ) : IRequest<bool>;
+}
