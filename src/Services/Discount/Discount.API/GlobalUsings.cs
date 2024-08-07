708ecfa786b01b6146d404b94988b05465cdf858
Minh Vương <hardingadonis@gmail.com>
2024-08-07 15:44:44 +0700

    [Add] Create program file & global using

---
+++ src/Services/Discount/Discount.API/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Discount.API.Services;
+global using Discount.Application.Commands;
+global using Discount.Application.Extensions;
+global using Discount.Application.Queries;
+global using Discount.Grpc.Protos;
+global using Discount.Infrastructure.Extensions;
+global using Grpc.Core;
+global using MediatR;
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,25 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
+await builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGrpcService<DiscountService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
