c4f84674800b8ac4f218429a0bb1bc4a730a3d7a
Minh Vương <hardingadonis@gmail.com>
2024-08-07 14:12:54 +0700

    [Add] Add `DiscountNotFoundException` exception

---
+++ src/Services/Discount/Discount.Application/Exceptions/DiscountNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace Discount.Application.Exceptions
+{
+    public class DiscountNotFoundException : RpcException
+    {
+        public string ProductName { get; private set; }
+
+        public DiscountNotFoundException(string productName)
+            : base(new Status(StatusCode.NotFound, $"Discount with ProductName: {productName} was not found"))
+        {
+            ProductName = productName;
+        }
+    }
+}
