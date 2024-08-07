ca35a9249211d004ce4b9c825bd9f089009a8aeb
Minh Vương <hardingadonis@gmail.com>
2024-08-07 14:12:25 +0700

    [Add] Implement mapper for discount

---
+++ src/Services/Discount/Discount.Application/Mappers/DiscountMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace Discount.Application.Mappers
+{
+    public class DiscountMapper
+    {
+        private readonly static Lazy<IMapper> _lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod?.IsPublic == true || p.GetMethod?.IsAssembly == true;
+
+                cfg.AddProfile<DiscountMappingProfile>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => _lazy.Value;
+    }
+}
---
+++ src/Services/Discount/Discount.Application/Mappers/DiscountMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Domain.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Application.Mappers
+{
+    public class DiscountMappingProfile : Profile
+    {
+        public DiscountMappingProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
