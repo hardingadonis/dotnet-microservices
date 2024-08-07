b32e46e7394114a7967ce271c59c9750b8ac6bfa
Minh Vương <hardingadonis@gmail.com>
2024-08-07 14:15:23 +0700

    [Add] Implement `CreateDiscountException` exception

---
+++ src/Services/Discount/Discount.Application/Exceptions/CreateDiscountException.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace Discount.Application.Exceptions
+{
+    public class CreateDiscountException : RpcException
+    {
+        public string ProductName { get; private set; }
+
+        public CreateDiscountException(string productName)
+            : base(new Status(StatusCode.AlreadyExists, $"Discount with ProductName: {productName} was not created"))
+        {
+            ProductName = productName;
+        }
+    }
+}
