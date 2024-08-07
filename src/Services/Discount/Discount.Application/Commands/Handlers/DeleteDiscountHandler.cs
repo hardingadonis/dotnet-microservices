55cb3d563c10652a5eca8eda4c79b0ecdee95ce9
Minh Vương <hardingadonis@gmail.com>
2024-08-07 14:37:16 +0700

    [Add] Implement delete discount command

---
+++ src/Services/Discount/Discount.Application/Commands/Handlers/DeleteDiscountHandler.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Application.Exceptions;
+using Discount.Domain.Repositories;
+using MediatR;
+
+namespace Discount.Application.Commands.Handlers
+{
+    public class DeleteDiscountHandler : IRequestHandler<DeleteDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DeleteDiscountHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+        }
+
+        public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var deleted = await _discountRepository.DeleteDiscount(request.ProductName);
+
+            return deleted ? deleted : throw new DiscountNotFoundException(request.ProductName);
+        }
+    }
+}
