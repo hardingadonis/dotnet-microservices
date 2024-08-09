02113af5d25e1150af1f5779a1afc3858f7dbf6e
Minh Vương <hardingadonis@gmail.com>
2024-08-09 10:21:28 +0700

    [Add] Create order context & seed data

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/OrderingContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderingContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderingContext(DbContextOptions<OrderingContext> options)
+            : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        entry.Entity.CreatedBy = "adonis";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        entry.Entity.LastModifiedBy = "adonis";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/OrderingContextSeed.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using Polly;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderingContextSeed
+    {
+        protected OrderingContextSeed()
+        {
+        }
+
+        public static async Task SeedAsync(OrderingContext orderContext, ILogger<OrderingContextSeed> logger)
+        {
+            var policy = Policy.Handle<DbUpdateException>()
+                .WaitAndRetryAsync(
+                    retryCount: 5,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (exception, timeSpan, retry, ctx) =>
+                    {
+                        logger.LogError(
+                            "[OrderContextSeed] Exception {message} with attempt {retry} of {policyKey}", exception.Message, retry, ctx.PolicyKey);
+                    }
+                );
+
+            await policy.ExecuteAsync(async () =>
+            {
+                if (!await orderContext.Orders.AnyAsync())
+                {
+                    await orderContext.Orders.AddRangeAsync(GetPreconfiguredOrders());
+
+                    await orderContext.SaveChangesAsync();
+
+                    logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderingContext).Name);
+                }
+            });
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return
+            [
+                new Order()
+                {
+                    UserName = "adonis",
+                    FirstName = "Minh",
+                    LastName = "Vương",
+                    Email = "hardingadonis@gmail.com",
+                    AddressLine = "Quy Nhơn",
+                    Country = "Vietnam",
+                    TotalPrice = 350,
+                    ZipCode = "123456",
+
+                    CardName = "MasterCard",
+                    CardNumber = "1234567890",
+                    Expiration = "12/24",
+                    CVV = "123",
+                    PaymentMethod = PaymentMethod.CreditCard
+                }
+            ];
+        }
+    }
+}
