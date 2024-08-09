63b886cda34014ab8e1831990e5a11df447824cf
Minh Vương <hardingadonis@gmail.com>
2024-08-09 14:45:58 +0700

    [Add] Create order response

---
+++ src/Services/Ordering/Ordering.Application/Responses/OrderResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace Ordering.Application.Responses
+{
+    public class OrderResponse
+    {
+        public long Id { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string AddressLine { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public string ZipCode { get; set; } = string.Empty;
+
+        public string CardName { get; set; } = string.Empty;
+
+        public string CardNumber { get; set; } = string.Empty;
+
+        public string Expiration { get; set; } = string.Empty;
+
+        public string CVV { get; set; } = string.Empty;
+
+        public int PaymentMethod { get; set; }
+    }
+}
