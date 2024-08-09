99a77456f4da8a20c6ce8b5037f99b3170b930bf
Minh Vương <hardingadonis@gmail.com>
2024-08-09 09:25:15 +0700

    [Add] Implement entities for ordering service

---
+++ src/Services/Ordering/Ordering.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; protected set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public string? LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿namespace Ordering.Domain.Entities
+{
+    public enum PaymentMethod : byte
+    {
+        Cash = 1,
+        CreditCard = 2,
+        DebitCard = 3
+    }
+
+    public class Order : BaseEntity
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string AddressLine { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public string ZipCode { get; set; } = string.Empty;
+
+        public string CardName { get; set; } = string.Empty;
+
+        public string CardNumber { get; set; } = string.Empty;
+
+        public string Expiration { get; set; } = string.Empty;
+
+        public string CVV { get; set; } = string.Empty;
+
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
