f8ade933d289fa579f06dfd64eca75890a3e6ec7
Minh Vương <hardingadonis@gmail.com>
2024-08-09 14:45:40 +0700

    [Add] Create mapper & mapper profile

---
+++ src/Services/Ordering/Ordering.Application/Mappers/OrderingMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace Ordering.Application.Mappers
+{
+    public static class OrderingMapper
+    {
+        private readonly static Lazy<IMapper> _lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod?.IsPublic == true || p.GetMethod?.IsAssembly == true;
+
+                cfg.AddProfile<OrderingMappingProfile>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => _lazy.Value;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Mappers/OrderingMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappers
+{
+    public class OrderingMappingProfile : Profile
+    {
+        public OrderingMappingProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
