fc5116048603bafeff57cb9328868eb15f99970d
Minh Vương <hardingadonis@gmail.com>
2024-08-09 09:32:48 +0700

    [Add] Create interface repository for ordering service

---
+++ src/Services/Ordering/Ordering.Domain/Repositories/IAsyncRepository.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Ordering.Domain.Repositories
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+        Task<T?> GetByIdAsync(int id);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Domain.Entities;
+
+namespace Ordering.Domain.Repositories
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
