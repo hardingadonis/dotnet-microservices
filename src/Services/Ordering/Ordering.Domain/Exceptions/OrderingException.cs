4dae5a410cecb3e03f4c524b9f22a9d5f6159256
Minh Vương <hardingadonis@gmail.com>
2024-08-09 09:32:27 +0700

    [Add] Create ordering exception

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/OrderingException.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Exceptions
+{
+    public class OrderingException : Exception
+    {
+        public OrderingException()
+        {
+        }
+
+        public OrderingException(string message)
+            : base(message)
+        {
+        }
+
+        public OrderingException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
