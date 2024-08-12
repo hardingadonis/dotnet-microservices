6b4b055ec198c1ef918acb0af5ddb902278515e7
Minh Vương <hardingadonis@gmail.com>
2024-08-12 16:59:41 +0700

    [Add] Implement exceptions for handling request

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/RequestHandlingException.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    public class RequestHandlingException : OrderingException
+    {
+        public string RequestName { get; }
+
+        public RequestHandlingException(string requestName, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            RequestName = requestName;
+        }
+    }
+}
