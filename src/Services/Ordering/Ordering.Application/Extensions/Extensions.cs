48ae5790bd220ebd2b0247bab4dd524e27aa9356
Minh Vương <hardingadonis@gmail.com>
2024-08-12 17:02:04 +0700

    [Add] Implement extension method

---
+++ src/Services/Ordering/Ordering.Application/Extensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Extensions).Assembly));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+            return services;
+        }
+    }
+}
